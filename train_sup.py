1785d2b0b1e658446244908fbb1cf55e787527e2
Anthony Cui <cuianthony@gmail.com>
2019-12-08 13:51:03 -0500

    computing A

--- train_sup.py
+++ train_sup.py
@@ -64,7 +64,7 @@ def train_supervised(states, symbols, labelled_sequences, estimator=None, extra_
     alpha=0.5
     all_hidden_states = A.conditions()
     num_states = len(all_hidden_states)
-    T=200
+    T=10
     p_qt = np.zeros((T,num_states))
     for i,l in enumerate(all_hidden_states):
         p_qt[0,i] = pi.prob(l)
@@ -104,9 +104,48 @@ def train_supervised(states, symbols, labelled_sequences, estimator=None, extra_
                     if abs(b_candidate - 1.) <= 0.1:
                         b_prob[i,j] = b_candidate
     b_prob = b_prob / b_prob.sum(axis=1)[...,None]
-    import pdb;pdb.set_trace()
     print(b_prob[0])
-    
+
+    # Computing A
+    a_prob = np.zeros((num_states,num_states))
+    for i,l in enumerate(all_hidden_states):
+        for j,m in enumerate(all_hidden_states):
+            a_prob[i, j] = A.get(l).prob(m)
+
+    partials = np.zeros((T, num_states, num_states,num_states))
+    for t in range(2, T):
+        for i in range(len(all_hidden_states)):
+            for l in range(len(all_hidden_states)):
+                for m in range(len(all_hidden_states)):
+                    if t==2:
+                        if m==i:
+                            partials[t,i,l,m] = p_qt[0,l]
+                    else:
+                        for j in range(len(all_hidden_states)):
+                            partials[t,i,l,m] += partials[t-1,j,l,m]*a_prob[j,i]
+                        if m==i:
+                            partials[t,i,l,m] += p_qt[t-1, l]
+
+    beta = 10
+    for l,l_v in enumerate(all_hidden_states):
+        for m,m_v in enumerate(all_hidden_states):
+            if transitions.get(l_v).get(m_v):
+                a_prob[l,m] = (-alpha*transitions.get(l_v).get(m_v))
+                denom = 0
+
+                for t in range(T):
+                    for i in range(len(all_hidden_states)):
+                        for k in range(len(symbols)):
+                            if b_prob[i,k] != 0:
+                                denom += b_prob[i,k]*np.log(b_prob[i,k])*partials[t,i,l,m]
+                denom *= (1-alpha)
+                denom += beta
+                a_prob[l,m] /= denom
+
+
+    import pdb;pdb.set_trace()
+
+
     return hmm.HiddenMarkovModelTagger(symbols, states, A, B, pi)
 
 
